3f94a84b3c90458957a9140e8bb9390d7c5ec537
heintzz <mhasnanr@gmail.com>
2023-12-05 09:00:48 +0700

    fix: edit goal

--- src/app/(dashboard)/goal/page.jsx
+++ src/app/(dashboard)/goal/page.jsx
@@ -1,30 +1,33 @@
 "use client";
 
-import Image from "next/image";
-import { useRouter } from "next/navigation";
-import { useEffect, useState, useRef } from "react";
-import { useSearchParams } from "next/navigation";
-import { useForm, Controller } from "react-hook-form";
-import { goalServices } from "@/services/goal.services";
-import { convertNumberToCurrencyFormat } from "@/helpers/helper";
-import { useAlertStore } from "@/stores/alert.store";
-import { MdModeEdit, MdDeleteForever } from "react-icons/md";
-import { FaPlus } from "react-icons/fa";
-import { Fragment } from "react";
-import Modal from "@/components/Modal";
 import AlertResponse from "@/components/AlertResponse";
 import DeleteConfirmation from "@/components/DeleteConfirmation";
+import Modal from "@/components/Modal";
+import ValidationMessage from "@/components/ValidationMessage";
+import { convertNumberToCurrencyFormat } from "@/helpers/helper";
+import { goalServices } from "@/services/goal.services";
+import { useAlertStore } from "@/stores/alert.store";
+import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { FaPlus } from "react-icons/fa";
+import { MdDeleteForever, MdModeEdit } from "react-icons/md";
 
 const GoalPage = () => {
-  const { control, handleSubmit, reset } = useForm();
+  const {
+    handleSubmit,
+    reset,
+    register,
+    formState: { errors },
+  } = useForm();
   const router = useRouter();
   const [goalData, setGoalData] = useState([]);
-  const [isDeleteConfirmationVisible, setDeleteConfirmationVisible] =
-    useState(false);
+  const [isDeleteConfirmationVisible, setDeleteConfirmationVisible] = useState(false);
   const setAlert = useAlertStore((state) => state.setAlert);
-  const searchParams = useSearchParams();
-  const id = searchParams.get("id");
+  const [showModal, setShowModal] = useState(false);
+  const [editId, setEditId] = useState("");
 
   useEffect(() => {
     // Fetch goal data and update the state
@@ -65,15 +68,15 @@ const GoalPage = () => {
     })();
   };
 
-  const [showModal, setShowModal] = useState(false);
-  const savingsAmount = searchParams.get("amount");
+  const openAddSavingAmount = (id) => {
+    setEditId(id);
+    setShowModal(true);
+  };
+
   const onSubmit = (data) => {
     (async () => {
-      const patchData = {
-        savingsAmount: data.amount,
-      };
       try {
-        const res = await goalServices.editGoalData(patchData, id);
+        const res = await goalServices.editGoalData(data, editId);
         if (res.success) {
           reset();
           router.push("/goal");
@@ -82,6 +85,7 @@ const GoalPage = () => {
             success: true,
             message: "add saving amount successfully",
           });
+          setShowModal(false);
         }
       } catch (error) {
         console.error(error);
@@ -92,6 +96,32 @@ const GoalPage = () => {
   return (
     <div className="pt-4 pb-8 lg:pb-24">
       <AlertResponse />
+      <Modal isVisible={showModal} onClose={() => setShowModal(false)}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <h3 className="text-xl text-center font-semibold mb-5">Add Saving Amount</h3>
+          <div className="mb-4">
+            <input
+              type="number"
+              className="input input-bordered focus:outline-black focus:border-none w-full"
+              {...register("savingsAmount", {
+                required: "Saving amount cannot be empty",
+                valueAsNumber: true,
+              })}
+            />
+            {errors?.savingsAmount ? (
+              <ValidationMessage>{errors?.savingsAmount.message}</ValidationMessage>
+            ) : null}
+          </div>
+          <div className="text-center">
+            <button
+              type="submit"
+              className="rounded-full text-white px-4 py-2 bg-main hover:bg-main-hover active:bg-main-active"
+            >
+              Add
+            </button>
+          </div>
+        </form>
+      </Modal>
       <div className="bg-white min-w-screen py-4 flex items-center justify-between">
         <h1 className="font-bold text-[32px] text-black ml-8">Goal</h1>
         <div className="flex space-x-4 mr-20">
@@ -105,35 +135,32 @@ const GoalPage = () => {
       </div>
       <div>
         {goalData.map((goal) => {
-          const percentage = Math.round(
-            (goal.savingsAmount / goal.goalPrice) * 100
-          );
+          let percentage = Math.round((goal.savingsAmount / goal.goalPrice) * 100);
+          const maxPercentage = 100;
+          percentage = percentage > maxPercentage ? maxPercentage : percentage;
           const heightOfCompletion =
             percentage === 100 ? "h-full rounded-[10px]" : "rounded-b-[10px]";
+
           return (
             <div
               key={goal._id}
-              className="bg-white mt-4 lg:mt-10 mb-4 lg:mb-10 rounded-xl mx-2 sm:mx-4 lg:mx-8 py-4 lg:py-8 flex flex-col sm:flex-row items-center justify-between"
+              className="bg-white mt-4 lg:mt-10 mb-4 sm:pr-10 px-5 lg:mb-10 rounded-xl mx-4 lg:mx-8 py-4 lg:py-8 flex flex-col sm:px sm:flex-row sm:justify-between"
             >
               <div
                 key={goal._id}
-                className="flex flex-col items-center sm:flex-row sm:items-start mx-2 sm:mx-4 lg:mx-10 my-2 lg:my-0"
+                className="flex flex-col sm:flex-row sm:items-start mr-2 sm:mr-4 lg:mr-10 my-2 lg:my-0"
               >
                 <Image
                   src={goal.goalImage}
                   alt="Goal"
                   width={100}
                   height={100}
-                  className="rounded-full sm:mr-4 lg:mr-8 mb-4 sm:mb-0"
+                  className="rounded-full hidden sm:block sm:mr-4 lg:mr-8 mb-4 sm:mb-0"
                 />
-                <div className="ml-0 sm:ml-4 mt-4 sm:mt-0 text-center sm:text-left">
-                  <h1 className="font-bold text-[30px] text-black">
-                    {goal.goalName}
-                  </h1>
+                <div className="ml-0 sm:ml-4 text-left">
+                  <h1 className="font-bold text-[30px] text-black">{goal.goalName}</h1>
                   <p className="text-lg font-semibold">Description</p>
-                  <span className="text-[15px] text-black">
-                    {goal.goalDescription}
-                  </span>
+                  <span className="text-[15px] text-black">{goal.goalDescription}</span>
                   <p className="text-lg font-semibold">Store</p>
                   <Link href={goal.goalStore} className="text-black underline">
                     {goal.goalStore}
@@ -144,7 +171,7 @@ const GoalPage = () => {
                   </span>
                 </div>
               </div>
-              <div className="mr-20">
+              <div>
                 <h1 className="font-bold mb-5">
                   <p>Saving Amount</p>
                   <span className="text-main">
@@ -155,60 +182,23 @@ const GoalPage = () => {
                 <div className="flex">
                   <div className="font-bold relative text-[40px] grid w-32 h-32 border-main border-2 rounded-xl place-items-center">
                     <div
-                      className={`bg-green-200 absolute ${heightOfCompletion} w-full bottom-0`}
+                      className={`bg-green-200 ${heightOfCompletion} w-full bottom-0 ${
+                        percentage < 7 ? "hidden" : "absolute"
+                      }`}
                       style={{ height: `${percentage}%` }}
                     ></div>
-                    <span className="text-black">{percentage}%</span>
+                    <p className="text-black relative z-[1]">{percentage}%</p>
                   </div>
                   <div className="flex flex-col justify-between ml-4">
-                    <div>
-                      <Fragment>
-                        <button
-                          className="bg-main rounded-full p-2"
-                          onClick={() => setShowModal(true)}
-                        >
-                          <FaPlus size="1.45em" fill="white" />
-                        </button>
-                        <Modal
-                          isVisible={showModal}
-                          onClose={() => setShowModal(false)}
-                        >
-                          <form onSubmit={handleSubmit(onSubmit)}>
-                            <h3
-                              className="text-xl font-semibold text-black-900 mb-5"
-                              style={{ textAlign: "center" }}
-                            >
-                              Add Saving Amount
-                            </h3>
-                            <div className="mb-4">
-                              <Controller
-                                name="amount"
-                                control={control}
-                                defaultValue={savingsAmount}
-                                render={({ field }) => (
-                                  <input
-                                    {...field}
-                                    type="number"
-                                    className="w-full py-2 px-2 rounded-md bg-white outline outline-1 mt-2"
-                                  />
-                                )}
-                              />
-                            </div>
-                            <div className="text-center">
-                              <button
-                                type="submit"
-                                className="rounded-full text-white px-4 py-2 bg-main hover:bg-main-hover active:bg-main-active"
-                              >
-                                Add
-                              </button>
-                            </div>
-                          </form>
-                        </Modal>
-                      </Fragment>
-                    </div>
+                    <button
+                      className="bg-main rounded-full p-2"
+                      onClick={() => openAddSavingAmount(goal._id)}
+                    >
+                      <FaPlus size="1.45em" fill="white" />
+                    </button>
                     <Link
                       href={{
-                        pathname: "/goal/Edit",
+                        pathname: "/goal/edit",
                         query: {
                           id: goal._id,
                           name: goal.goalName,
@@ -222,10 +212,7 @@ const GoalPage = () => {
                         <MdModeEdit size="1.45em" fill="white" />
                       </button>
                     </Link>
-                    <button
-                      onClick={showDeleteConfirmation}
-                      className="bg-error rounded-full p-2"
-                    >
+                    <button onClick={showDeleteConfirmation} className="bg-error rounded-full p-2">
                       <MdDeleteForever size="1.45em" fill="white" />
                     </button>
                   </div>
